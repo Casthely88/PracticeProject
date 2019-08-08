aed5deb1e7bd9569dc7b7afefcba2afe4a57222c
Casthely88 <53665749+Casthely88@users.noreply.github.com>
2019-08-08 19:46:43 -0400

    Add files via upload

---
+++ Source/PluginProcessor.h
@@ -0,0 +1,68 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+/*
+TODO:
+click anywhere on the window, and play a note
+if you click and drag, it'll change the pitch of the note
+Should we play a sound?
+
+*/
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+//==============================================================================
+/**
+*/
+class PracticeProjectAudioProcessor  : public AudioProcessor
+{
+public:
+    //==============================================================================
+    PracticeProjectAudioProcessor();
+    ~PracticeProjectAudioProcessor();
+
+    //==============================================================================
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override;
+
+   #ifndef JucePlugin_PreferredChannelConfigurations
+    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
+   #endif
+
+    void processBlock (AudioBuffer<float>&, MidiBuffer&) override;
+
+    //==============================================================================
+    AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override;
+
+    //==============================================================================
+    const String getName() const override;
+
+    bool acceptsMidi() const override;
+    bool producesMidi() const override;
+    bool isMidiEffect() const override;
+    double getTailLengthSeconds() const override;
+
+    //==============================================================================
+    int getNumPrograms() override;
+    int getCurrentProgram() override;
+    void setCurrentProgram (int index) override;
+    const String getProgramName (int index) override;
+    void changeProgramName (int index, const String& newName) override;
+
+    //==============================================================================
+    void getStateInformation (MemoryBlock& destData) override;
+    void setStateInformation (const void* data, int sizeInBytes) override;
+
+private:
+	bool shouldPlaySound = true;
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PracticeProjectAudioProcessor)
+};
